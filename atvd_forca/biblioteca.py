e951f2679c0839843a0f8e23844d2cf6c9a315f4
Emilly de Lima <emillylpn123@gmail.com>
2023-11-07 16:34:03 +0000

    Jogo da Forca

--- atvd_forca/biblioteca.py
+++ atvd_forca/biblioteca.py
@@ -1,47 +1,52 @@
 import random
 
-
 def JogoGeral():
+    def escolher_palavra():
+        palavras = ["brasil", "bola", "computador"]
+        return random.choice(palavras)
+    chances = 4
+    letras_usadas = []
+    palavra_secreta = escolher_palavra()
 
-    ganhou= False
-    palavra="brasil"
-
-    chances=4
-    letras_usuarios=[]
+    def dicas():
+        if chances==1 and palavra_secreta=="brasil":
+            print("Última chance! Dica: Maior país da América Latina.")
+        if chances==1 and palavra_secreta=="computador":
+            print("Última chance! Dica: Objeto principal para Programar.")
+        if chances==1 and palavra_secreta=="bola":
+            print("Última chance! Dica: Objeto para jogar Futebol.")
 
     while True:
-        for letra in palavra:
-            if letra.lower() in letras_usuarios:
+        for letra in palavra_secreta:
+            if letra in letras_usadas:
                 print(letra, end=" ")
             else:
                 print("_", end=" ")
 
         print(f"Você tem {chances} tentativas.")
 
-        tentativa = input("Digite uma letra para advinhar: ")
-        letras_usuarios.append(tentativa.lower())
-
+        tentativa = input("Digite uma letra para adivinhar: ").lower()
+        letras_usadas.append(tentativa)
 
         ganhou = True
-        for letra in palavra:
-            if letra.lower() not in letras_usuarios:
-                ganhou=False
+        for letra in palavra_secreta:
+            if letra not in letras_usadas:
+                ganhou = False
 
-        if tentativa.lower() not in palavra.lower():
-            chances-=1
+        if tentativa not in palavra_secreta:
+            chances -= 1
 
-        if chances==1:
-            print("Última tentativa!Dica: Maior país da América Latina!")
+        if chances ==1:
+            print(dicas)
 
-        if chances==0 or ganhou:
-            print(f"Suas tentativas acabaram! a palavra era: {palavra}")
+        if chances == 0 or ganhou:
+            print(f"Suas tentativas acabaram! A palavra era: {palavra_secreta}")
             break
 
-
     if ganhou:
-        print(f"Parabéns! Você venceu.")
-
+        print("Parabéns! Você venceu.")
     else:
-        print(f"Você perdeu! A palavra era {palavra}")
+        print(f"Você perdeu! A palavra era {palavra_secreta}")
+
 
 
